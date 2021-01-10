d4cc731b4a4de8bfda096f999aef1e2242440551
Nareloke <lucas_anielo@hotmail.com>
2021-01-10 10:01:16 -0300

    InMemoryTokenDAO implementado. Novo id para a entity Token criado.

--- src/main/java/br/edu/ifsp/application/main/repository/InMemoryTokenDAO.java
+++ src/main/java/br/edu/ifsp/application/main/repository/InMemoryTokenDAO.java
@@ -3,37 +3,53 @@ package br.edu.ifsp.application.main.repository;
 import br.edu.ifsp.domain.entities.usuario.Token;
 import br.edu.ifsp.domain.usecases.usuario.TokenDAO;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class InMemoryTokenDAO implements TokenDAO {
+    private static final Map<Integer, Token> db = new LinkedHashMap<>();
+    private static int idCounter;
+
     @Override
-    public Integer create(Token type) {
-        return null;
+    public Integer create( Token token ) {
+        idCounter++;
+        token.setId( idCounter );
+        db.put( idCounter, token );
+        return idCounter;
     }
 
     @Override
-    public Optional<Token> findOne(Integer key) {
+    public Optional<Token> findOne( Integer key ) {
+        if ( db.containsKey( key ) )
+            return Optional.of( db.get( key ) );
         return Optional.empty();
     }
 
     @Override
     public List<Token> findAll() {
-        return null;
+        return new ArrayList<>( db.values() );
     }
 
     @Override
-    public boolean update(Token type) {
+    public boolean update( Token token ) {
+        Integer id = token.getId();
+        if ( db.containsKey( id ) ) {
+            db.replace( id, token );
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean deleteByKey(Integer key) {
+    public boolean deleteByKey( Integer key ) {
+        if ( db.containsKey( key ) ) {
+            db.remove( key );
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean delete(Token type) {
-        return false;
+    public boolean delete( Token token ) {
+        return deleteByKey( token.getId() );
     }
 }
