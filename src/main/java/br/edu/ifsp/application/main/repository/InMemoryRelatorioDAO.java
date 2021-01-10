b7e9bb1766646fe320066a1526f563edec9bdd5a
Manu <emanoelasimao@gmail.com>
2021-01-10 11:00:47 -0300

    Criados DAOS que faltavam

---
+++ src/main/java/br/edu/ifsp/application/main/repository/InMemoryRelatorioDAO.java
@@ -0,0 +1,39 @@
+package br.edu.ifsp.application.main.repository;
+
+import br.edu.ifsp.domain.entities.relatorio.Relatorio;
+import br.edu.ifsp.domain.usecases.relatorio.RelatorioDAO;
+
+import java.util.List;
+import java.util.Optional;
+
+public class InMemoryRelatorioDAO implements RelatorioDAO {
+    @Override
+    public Integer create(Relatorio type) {
+        return null;
+    }
+
+    @Override
+    public Optional<Relatorio> findOne(Integer key) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Relatorio> findAll() {
+        return null;
+    }
+
+    @Override
+    public boolean update(Relatorio type) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteByKey(Integer key) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Relatorio type) {
+        return false;
+    }
+}
