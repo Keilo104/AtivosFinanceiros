26b3e03a42f7c41cc25c889c045c4fca228445c6
Manu <emanoelasimao@gmail.com>
2021-01-10 09:53:18 -0300

    implementado o InMemoryUsuarioDAO

--- src/main/java/br/edu/ifsp/application/main/repository/InMemoryUsuarioDAO.java
+++ src/main/java/br/edu/ifsp/application/main/repository/InMemoryUsuarioDAO.java
@@ -3,42 +3,70 @@ package br.edu.ifsp.application.main.repository;
 import br.edu.ifsp.domain.entities.usuario.Usuario;
 import br.edu.ifsp.domain.usecases.usuario.UsuarioDAO;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class InMemoryUsuarioDAO implements UsuarioDAO{
+
+    private static final Map<String, Usuario> db = new LinkedHashMap<>();
+
     @Override
-    public int checkLogin(Usuario usuario) {
-        return 0;
+    public boolean checkLogin(Usuario usuario) {
+        Usuario u = findOneByEmail(usuario.getEmail()).orElse(null);
+        if(u != null){
+            return (u.getSenha().equals(usuario.getSenha()));
+        }
+        return false;
     }
 
     @Override
-    public Integer create(Usuario type) {
-        return null;
+    public String create(Usuario usuario){
+        db.put(usuario.getCpf(), usuario);
+        return usuario.getCpf();
     }
 
     @Override
-    public Optional<Usuario> findOne(Integer key) {
+    public Optional<Usuario> findOne(String key) {
+        if(db.containsKey(key)){
+            return Optional.of(db.get(key));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Usuario> findOneByEmail(String email){
+        for(Usuario u : db.values()) {
+            if(u.getEmail().equals(email)){
+                return Optional.of(u);
+            }
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Usuario> findAll() {
-        return null;
+        return new ArrayList<>(db.values());
     }
 
     @Override
-    public boolean update(Usuario type) {
+    public boolean update(Usuario usuario){
+        String cpf = usuario.getCpf();
+        if (db.containsKey(cpf)) {
+            db.replace(cpf, usuario);
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean deleteByKey(Integer key) {
+    public boolean deleteByKey(String key) {
+        if(db.containsKey(key)) {
+            db.remove(key);
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean delete(Usuario type) {
-        return false;
+    public boolean delete(Usuario usuario){
+        return deleteByKey(usuario.getCpf());
     }
 }
--- src/main/java/br/edu/ifsp/domain/usecases/usuario/UsuarioDAO.java
+++ src/main/java/br/edu/ifsp/domain/usecases/usuario/UsuarioDAO.java
@@ -3,6 +3,6 @@ package br.edu.ifsp.domain.usecases.usuario;
 import br.edu.ifsp.domain.entities.usuario.Usuario;
 import br.edu.ifsp.domain.usecases.utils.DAO;
 
-public interface UsuarioDAO extends DAO<Usuario, Integer> {
-    int checkLogin( Usuario usuario );
+public interface UsuarioDAO extends DAO<Usuario, String> {
+    boolean checkLogin( Usuario usuario );
 }
