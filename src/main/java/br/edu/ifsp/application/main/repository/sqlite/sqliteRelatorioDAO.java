2cebbf80753e022272cf77b1eccd88a47bcba671
Manu <emanoelasimao@gmail.com>
2021-01-30 19:25:14 -0300

    daos de relatório

--- src/main/java/br/edu/ifsp/application/main/repository/sqlite/sqliteRelatorioDAO.java
+++ src/main/java/br/edu/ifsp/application/main/repository/sqlite/sqliteRelatorioDAO.java
@@ -1,39 +1,103 @@
 package br.edu.ifsp.application.main.repository.sqlite;
 
+import br.edu.ifsp.domain.entities.grupo.TipoGrupoEnum;
 import br.edu.ifsp.domain.entities.relatorio.Relatorio;
 import br.edu.ifsp.domain.usecases.relatorio.RelatorioDAO;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class sqliteRelatorioDAO implements RelatorioDAO {//TODO
     @Override
-    public Integer create(Relatorio type) {
+    public Integer create(Relatorio relatorio) {
+        String sql = "INSERT INTO RELATORIO(dataImpressao, categoria) VALUES(?,?);";
+        try (PreparedStatement stat = ConnectionFactory.createPreparedStatement(sql)) {
+            stat.setString(1, relatorio.getDataImpressao().toString());
+            stat.setString(2, relatorio.getCategoria().getString());
+
+            stat.execute();
+            ResultSet resultSet = stat.getGeneratedKeys();
+            int idCriado = resultSet.getInt(1);
+            return idCriado;
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return null;
     }
 
+    private Relatorio resultSetToEntity(ResultSet rs) throws SQLException {
+        int id = rs.getInt("id");
+        LocalDate dataImpressao = LocalDate.parse(rs.getString("dataImpressao"));
+        String categoria = rs.getString("categoria");
+
+        return new Relatorio(id, dataImpressao, TipoGrupoEnum.getValueByString(categoria));
+    }
+
     @Override
-    public Optional<Relatorio> findOne(Integer key) {
-        return Optional.empty();
+    public Optional<Relatorio> findOne(Integer id) {
+        String sql = "SELECT * FROM RELATORIO WHERE id = ?";
+        Relatorio relatorio = null;
+        try (PreparedStatement stat = ConnectionFactory.createPreparedStatement(sql)) {
+            stat.setInt(1, id);
+            ResultSet rs = stat.executeQuery();
+
+            if(rs.next()) {
+                relatorio = resultSetToEntity(rs);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return Optional.ofNullable(relatorio);
     }
 
     @Override
     public List<Relatorio> findAll() {
-        return null;
+        String sql = "SELECT * FROM RELATORIO;";
+        List<Relatorio> relatorios = new ArrayList<>();
+        try (PreparedStatement stat = ConnectionFactory.createPreparedStatement(sql)) {
+            ResultSet rs = stat.executeQuery();
+            while(rs.next()) {
+                Relatorio relatorio = resultSetToEntity(rs);
+                relatorios.add(relatorio);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return relatorios;
     }
 
     @Override
-    public boolean update(Relatorio type) {
+    public boolean update(Relatorio relatorio) {
         return false;
     }
 
     @Override
-    public boolean deleteByKey(Integer key) {
+    public boolean deleteByKey(Integer id) {
+        String sql = "DELETE FROM RELATORIO WHERE id = ?;";
+        try (PreparedStatement stat = ConnectionFactory.createPreparedStatement(sql)) {
+            stat.setInt(1, id);
+            stat.execute();
+            return true;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return false;
     }
 
     @Override
-    public boolean delete(Relatorio type) {
-        return false;
+    public boolean delete(Relatorio relatorio) {
+        if(relatorio == null )
+            throw new IllegalArgumentException("Grupo cannot be null");
+        return deleteByKey(relatorio.getId());
     }
 }
--- src/main/java/br/edu/ifsp/domain/usecases/relatorio/RelatorioPeriodoDAO.java
+++ src/main/java/br/edu/ifsp/domain/usecases/relatorio/RelatorioPeriodoDAO.java
@@ -1,7 +1,8 @@
 package br.edu.ifsp.domain.usecases.relatorio;
 
 import br.edu.ifsp.domain.entities.relatorio.Relatorio;
+import br.edu.ifsp.domain.entities.relatorio.RelatorioPeriodo;
 import br.edu.ifsp.domain.usecases.utils.DAO;
 
-public interface RelatorioPeriodoDAO extends DAO<Relatorio, Integer> {
+public interface RelatorioPeriodoDAO extends DAO<RelatorioPeriodo, Integer> {
 }
