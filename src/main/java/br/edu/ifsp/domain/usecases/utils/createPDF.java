775d4f7174011c49b8deb9eecbf4af34ad653a52
vitorlpp <vitorlpastore11@gmail.com>
2021-01-31 11:49:57 -0300

    cria pdf, implementar

---
+++ src/main/java/br/edu/ifsp/domain/usecases/utils/createPDF.java
@@ -0,0 +1,29 @@
+package br.edu.ifsp.domain.usecases.utils;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class createPDF {
+    public static void main(String[] argv) {
+        Document document = new Document();
+        try {
+
+            PdfWriter.getInstance(document, new FileOutputStream("relatorio.pdf"));
+            document.open();
+
+            // adicionando um parágrafo no documento
+            document.add(new Paragraph("Gerando PDF - Java"));
+            document.add(new Paragraph("teste324"));
+            document.add(new Paragraph("teste34324"));
+        }
+        catch(DocumentException | IOException de) {
+            System.err.println(de.getMessage());
+        }
+        document.close();
+    }
+}
