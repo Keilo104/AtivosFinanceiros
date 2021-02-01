a0b0a72482b4d764d8553a20c3b2dc70e7a29e89
Nareloke <lucas_anielo@hotmail.com>
2021-01-31 21:23:56 -0300

    Adicionando import

--- src/main/java/br/edu/ifsp/domain/controller/PainelController.java
+++ src/main/java/br/edu/ifsp/domain/controller/PainelController.java
@@ -2,10 +2,7 @@ package br.edu.ifsp.domain.controller;
 
 import br.edu.ifsp.domain.entities.grupo.Grupo;
 import br.edu.ifsp.domain.entities.usuario.Usuario;
-import br.edu.ifsp.domain.ui.JanelaAcompanharAcao;
-import br.edu.ifsp.domain.ui.JanelaCriarGrupo;
-import br.edu.ifsp.domain.ui.JanelaGrupo;
-import br.edu.ifsp.domain.ui.JanelaLogin;
+import br.edu.ifsp.domain.ui.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
